9a30ab07706b72598ac827bd3f8716c61a2034da
MatLukac <mat.lukac13@gmail.com>
2023-11-25 00:27:08 +0100

    add FinishRoundResult, updated TableAreaInterface and Game

--- src/main/java/sk/uniba/fmph/dcs/Game.java
+++ src/main/java/sk/uniba/fmph/dcs/Game.java
@@ -1,17 +1,39 @@
 package sk.uniba.fmph.dcs;
 
-import sun.jvm.hotspot.utilities.Observer;
+import java.util.ArrayList;
 
-import javax.swing.text.TabExpander;
-
-public class Game {
+public class Game implements GameInterface{
     private final BagInterface bag;
     private final TableAreaInterface tableArea;
     private final ObserverInterface gameObserver;
+    private final ArrayList<BoardInterface> boards;
+    private int playerId;
 
-    public Game(BagInterface bag, TableAreaInterface tableArea, ObserverInterface gameObserver){
+    public Game(BagInterface bag, TableAreaInterface tableArea, ObserverInterface gameObserver, ArrayList<BoardInterface> board){
         this.bag = bag;
         this.tableArea = tableArea;
         this.gameObserver = gameObserver;
+        this.boards = board;
+        playerId = 0;
+    }
+
+    @Override
+    public boolean take(int playerId, int sourceId, int idx, int destinationIdx) {
+        if(this.playerId != playerId) return false;
+        ArrayList<Tile> tiles = tableArea.take(sourceId, idx);
+        if(tiles == null) return false;
+        boards.get(playerId).put(destinationIdx, tiles);
+
+        if(!tableArea.isRoundEnd()) return true;
+
+        FinishRoundResult f = FinishRoundResult.NORMAL;
+        for (BoardInterface board : boards) if(board.finishRound() == FinishRoundResult.GAME_FINISHED) f = FinishRoundResult.GAME_FINISHED;
+
+        if(f == FinishRoundResult.GAME_FINISHED) {
+            for(BoardInterface board : boards) board.endGame();
+        }
+        else tableArea.startNewRound();
+
+        return true;
     }
 }
--- src/main/java/sk/uniba/fmph/dcs/TableAreaInterface.java
+++ src/main/java/sk/uniba/fmph/dcs/TableAreaInterface.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public interface TableAreaInterface {
     ArrayList<Tile> take(int sourceIdx, int idx);
+    boolean isRoundEnd();
     void startNewRound();
     String state();
 }
